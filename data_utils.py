4bfa690ac5d272f3f6c772e890e51c4cd35bd7d5
ZZSSCCC <44541038+ZZSSCCC@users.noreply.github.com>
2023-03-27 16:00:20 +0800

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -7,7 +7,7 @@ import torch
 from torchvision import transforms
 from torch.utils.data import DataLoader, RandomSampler, DistributedSampler, SequentialSampler
 
-from dataset import Blood_Data
+from dataset import B_Data
 # from autoaugment import AutoAugImageNetPolicy
 # from dataset_her import CubDataset
 import torchvision
@@ -17,50 +17,25 @@ logger = logging.getLogger(__name__)
 
 def get_loader(args):
 
-    if args.dataset == 'blood_17':
-        train_transform = transforms.Compose([transforms.Resize((240, 240)),
-                                              transforms.RandomCrop((224, 224)),
-                                              transforms.RandomHorizontalFlip(),
-                                              transforms.ToTensor(),
-                                              transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-        test_transform = transforms.Compose([transforms.Resize((240, 240)),
-                                             transforms.CenterCrop((224, 224)),
-                                             transforms.ToTensor(),
-                                             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-        trainset = Blood_Data('/data1/zhangsc/Projects/TransFG/train.txt',transform=train_transform)
-        testset = Blood_Data('/data1/zhangsc/Projects/TransFG/test.txt',transform=test_transform)
-    elif args.dataset == 'blood_15':
-        train_transform = transforms.Compose([transforms.Resize((224, 224)),
-                                              # transforms.RandomCrop((224, 224)),
-                                              transforms.RandomHorizontalFlip(p=0.5),
-                                              transforms.ToTensor(),
-                                              transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-        test_transform = transforms.Compose([transforms.Resize((224, 224)),
-                                             # transforms.CenterCrop((224, 224)),
-                                             transforms.ToTensor(),
-                                             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-        trainset = Blood_Data('/data2/Public_dataset/Peripheral_blood_15/train.txt',transform=train_transform,mode='train')
-        testset = Blood_Data('/data2/Public_dataset/Peripheral_blood_15/test.txt',transform=test_transform,mode='test')
-    else:
-        train_transform = transforms.Compose([
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Lambda(lambda x: F.pad(x.unsqueeze(0), (8, 8, 8, 8), mode='reflect').squeeze()),
-            transforms.ToPILImage(),
-            transforms.RandomCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-        ])
-        test_transform = transforms.Compose([
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ])
-        trainset = Blood_Data('/data1/syx/dataset/cell_anno_1115_global_text_jpg_split/cell_anno_1115_global_text_train.txt', transform=train_transform,
-                              mode='train')
-        testset = Blood_Data('/data1/syx/dataset/cell_anno_1115_global_text_jpg_split/cell_anno_1115_global_text_test.txt', transform=test_transform,
-                             mode='test')
+    train_transform = transforms.Compose([
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Lambda(lambda x: F.pad(x.unsqueeze(0), (8, 8, 8, 8), mode='reflect').squeeze()),
+        transforms.ToPILImage(),
+        transforms.RandomCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    ])
+    test_transform = transforms.Compose([
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    trainset = B_Data('/data1/syx/dataset/cell_anno_1115_global_text_jpg_split/cell_anno_1115_global_text_train.txt', transform=train_transform,
+                          mode='train')
+    testset = B_Data('/data1/syx/dataset/cell_anno_1115_global_text_jpg_split/cell_anno_1115_global_text_test.txt', transform=test_transform,
+                         mode='test')
     # train_sampler = RandomSampler(trainset) #if args.local_rank == -1 else DistributedSampler(trainset)
     # test_sampler = SequentialSampler(testset) #if args.local_rank == -1 else DistributedSampler(testset)
     train_loader = DataLoader(trainset,
